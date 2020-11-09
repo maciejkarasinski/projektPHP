9ca66f0a5aebee53b7a342f6caf3192476e16b01
maciejkarasinski <63544096+maciejkarasinski@users.noreply.github.com>
2020-11-09 21:18:33 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-  $host = "localhost";
-  $db_user = "root";
-  $db_password = "";
-  $db_name = "mustat";
+  $host = "r42ii9gualwp7i1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+  $db_user = "ahg4fafnipfb1qym";
+  $db_password = "zjonfw62ofa9s8hm";
+  $db_name = "fzo0s8uymo0bfxit";
 ?>
